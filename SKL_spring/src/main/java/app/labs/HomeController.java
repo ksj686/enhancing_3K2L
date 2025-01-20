406a0582494f6df85030caac4f432e1dd4cb377a
ksj686 <kim_se_joon@naver.com>
2025-01-20 16:27:05 +0900

    create spring project

---
+++ SKL_spring/src/main/java/app/labs/HomeController.java
@@ -0,0 +1,19 @@
+package app.labs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/jsphome")
+	public String jsphome(Model model) {
+		return "home";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "thymeleaf/home";
+	}
+}
---
+++ SKL_spring/src/main/java/app/labs/SklSpringApplication.java
@@ -0,0 +1,13 @@
+package app.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklSpringApplication.class, args);
+	}
+
+}
