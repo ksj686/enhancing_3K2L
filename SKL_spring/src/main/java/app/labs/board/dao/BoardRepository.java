20454c0ab26cba398f7c5eb8ea14c8a9dceaee0d
bornsoon <hyolim922@hotmail.com>
2025-01-28 20:06:10 +0900

    boardrepository

--- SKL_spring/src/main/java/app/labs/board/dao/BoardRepository.java
+++ SKL_spring/src/main/java/app/labs/board/dao/BoardRepository.java
@@ -1,8 +1,18 @@
 package app.labs.board.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import app.labs.board.model.Board;
 
 @Mapper
 public interface BoardRepository {
-	
+	List<Board> getBoardList(@Param("boardCategory") String boardCategory);
+	Board getBoardInfo(int boardId);
+	int createBoardId();
+	void createBoard(Board board);
+	void updateBoard(Board board);
+	// 삭제 기능은 없음 
 }
