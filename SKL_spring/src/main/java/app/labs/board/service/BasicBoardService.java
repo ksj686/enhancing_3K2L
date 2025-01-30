0475a413735746406e831dba6c80f298d2c971e3
bornsoon <hyolim922@hotmail.com>
2025-01-30 22:50:22 +0900

    board

--- SKL_spring/src/main/java/app/labs/board/service/BasicBoardService.java
+++ SKL_spring/src/main/java/app/labs/board/service/BasicBoardService.java
@@ -1,13 +1,41 @@
 package app.labs.board.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import app.labs.board.dao.BoardRepository;
+import app.labs.board.model.Board;
 
 @Service
 public class BasicBoardService implements BoardService {
 	
 	@Autowired
 	BoardRepository boardRepository;
+	
+	@Override
+	public List<Board> getBoardList(String boardCategory) {
+		return boardRepository.getBoardList(boardCategory);
+	}
+	
+	@Override
+	public Board getBoardInfo(int boardId) {
+		return boardRepository.getBoardInfo(boardId);
+	}
+	
+	@Override
+	public int createBoardId() {
+		return boardRepository.createBoardId();
+	}
+	
+	@Override
+	public void createBoard(Board board) {
+		boardRepository.createBoard(board);
+	}
+	
+	@Override
+	public void updateBoard(Board board) {
+		boardRepository.updateBoard(board);
+	}
 }
--- SKL_spring/src/main/java/app/labs/board/service/BoardService.java
+++ SKL_spring/src/main/java/app/labs/board/service/BoardService.java
@@ -1,5 +1,14 @@
 package app.labs.board.service;
 
-public interface BoardService {
+import java.util.List;
 
+import app.labs.board.model.Board;
+
+public interface BoardService {
+	List<Board> getBoardList(String boardCategory);
+	Board getBoardInfo(int boardId);
+	int createBoardId();
+	void createBoard(Board board);
+	void updateBoard(Board board);
+	// 삭제 기능은 없음 
 }
