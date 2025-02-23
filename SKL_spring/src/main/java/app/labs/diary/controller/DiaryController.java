d4568a0da5dd2408910816d824d52ef46c518ea0
bornsoon <hyolim922@hotmail.com>
2025-02-24 02:46:18 +0900

    일기 실시간 fetch 적용

--- SKL_spring/src/main/java/app/labs/diary/controller/DiaryController.java
+++ SKL_spring/src/main/java/app/labs/diary/controller/DiaryController.java
@@ -3,16 +3,14 @@ package app.labs.diary.controller;
 import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -240,7 +238,6 @@ public class DiaryController {
 	}
 
 
-	
 	@GetMapping("diary/delete")
 	public String deleteDiary(@RequestParam("diaryId") int diaryId, HttpServletRequest request,
 							  RedirectAttributes redirectAttributes) {
@@ -262,5 +259,16 @@ public class DiaryController {
 	}
 
 
-	
+	@GetMapping("/api/diary/emotion/{diaryId}")
+	@ResponseBody
+	public Map<String, String> getDiaryEmotion(@PathVariable("diaryId") int diaryId) {
+		Map<String, String> response = new HashMap<>();
+
+		Diary diary = diaryService.getDiaryInfo(diaryId);
+		String emotion = diary.getDiaryEmotion();
+        //response.put("emotion", emotion != null ? emotion : "");
+        response.put("emotion", emotion);
+
+        return response;
+	}
 }
